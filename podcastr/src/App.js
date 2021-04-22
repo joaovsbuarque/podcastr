c1c0552b84b9491ba903d819a1c188cc5d9fb2e4
joaovitor <joaobuarqueestudos@gmail.com>
2021-04-22 14:36:28 -0300

    wip nwl

---
+++ podcastr/src/App.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <> 
+    <Button title="Butão 1" />
+    <Button title="Butão 2" />
+    <Button title="Butão 3" />
+    <Button title="Butão 4"/>
+    <Button title="Butão 5"/>
+    </>
+  );
+}
+
+export default App;
